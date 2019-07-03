acdf163e42e88a07d717af02fa76d916e5bc19ee
jsaini <jsaini@hyperwallet.com>
2019-07-03 16:27:37 -0700

    removed conditional import

--- TransferMethod/Extensions/HyperwalletTransferMethodType.swift
+++ TransferMethod/Extensions/HyperwalletTransferMethodType.swift
@@ -16,10 +16,8 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import Foundation
-#if !COCOAPODS
 import Common
-#endif
+import Foundation
 import HyperwalletSDK
 
 extension HyperwalletTransferMethodType {
